058d03855b0a172f362f7b2a8d389921c24780a8
meuequalsD <nirmalkrishnav@gmail.com>
2020-05-20 15:20:39 +0530

    clear code

--- app.py
+++ app.py
@@ -6,16 +6,15 @@ from pathlib import Path
 src_dir = "source"
 dst_dir = "destination"
 extensions = [".jpg", ".png"]
-
-# for jpgFile in glob.iglob(os.path.join(src_dir, "*.*g")):
-    # shutil.copy(jpgFile, dst_dir)
+count = 0
 
 for src_dir, dirs, files in os.walk(src_dir, topdown=False):
     for name in files:
         if Path(name).suffix.lower() in extensions:
+            count= count+1
             shutil.copy(os.path.join(src_dir, name), os.path.join(dst_dir, name))
 
-print('All files copied successfully')
+print("Files copied " + str(count))
 
 
 
